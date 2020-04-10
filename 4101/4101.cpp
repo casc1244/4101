de3fcc9f61583179947dae8782b187362bf7159d
casc1244 <1356602646@qq.com>
2020-04-10 15:28:13 +0800

    4101.cpp

--- 4101/4101.cpp
+++ 4101/4101.cpp
@@ -1,20 +1,30 @@
-﻿// 4101.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-
+﻿#include <opencv2/opencv.hpp>
 #include <iostream>
 
+using namespace cv;
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+	Mat srcMat = imread("D:\\360downloads\\9.png");
+	Mat dstMat;
+	int width, height;
+	width = srcMat.cols;
+	height = srcMat.rows;
+
+	float angle = -10.0, scale = 1;
+	Point2f center(srcMat.cols*0.5, srcMat.rows*0.5);
+	Mat affine_matrix = getRotationMatrix2D(center, angle, scale);
+
+	float  cos = abs(affine_matrix.at<double>(0, 0)), sin = abs(affine_matrix.at<double>(0, 1));
+	int new_Width = int((height * sin) + (width * cos));
+	int new_Height = int((height * cos) + (width * sin));
+
+	affine_matrix.at<double>(1, 2) = affine_matrix.at<double>(1, 2) + (new_Width / 2) - width / 2;
+	affine_matrix.at<double>(0, 2) = affine_matrix.at<double>(0, 2) + (new_Height / 2) - height / 2;
+
+	warpAffine(srcMat, dstMat, affine_matrix, Size(new_Width, new_Height));
+	imshow("src", srcMat);
+	imshow("dst", dstMat);
+	waitKey(0);
+	return 0;
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
